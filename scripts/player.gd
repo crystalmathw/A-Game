b39e2d5919b4a0df0298ac7dcde95a328ad1b715
crystalmathw <121028354+crystalmathw@users.noreply.github.com>
2025-04-11 15:06:24 +0200

    Dying and Sprinting
    You can die now...  and Sprint... but mostly die

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,45 @@
 extends CharacterBody2D
 
+@export var playerHealth: float = 100
+@export var SPEED = 130.0
+@export var SPRINT = 2
 
-const SPEED = 130.0
 
+@onready var player: CharacterBody2D = $"."
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var timer: Timer = $Timer
+@onready var restart_timer: Timer = $RestartTimer
+@onready var hit_timer: Timer = $HitTimer
+@onready var death_text: Label = $DeathText
+@onready var animation: AnimationPlayer = $AnimationPlayer
 
-
-var playerHealth: float = 100
+var speedMultiplier = 1
 var lastDirection: float = 0
+var startingPosition: Vector2
+var zIndex
 
 func dead() -> void:
 	animated_sprite.play("death")
+	animation.play("DeathText FadeIn")
 	Engine.time_scale = 0.5
-	timer.start()
+	restart_timer.start()
+	set_physics_process(false)
 	
+func _on_restart_timer_timeout() -> void:
+	Engine.time_scale = 1
+	player.position = startingPosition
+	playerHealth = 100
+	set_physics_process(true)
+	animation.play("DeathText FadeOut")
+
+
 func playerTakeDamage(damage: float):
 	playerHealth = playerHealth - damage
-	
+	animated_sprite.modulate = Color(1, 0, 0, 1) 
+	hit_timer.start()
+
+func _on_hit_timer_timeout() -> void:
+	animated_sprite.modulate = Color(1, 1, 1, 1) 
+
 func getPlayerHealth() -> float:
 	return playerHealth
 
@@ -52,29 +74,36 @@ func movement() -> void:
 		elif direction_x != 0 and direction_y > 0:
 			animated_sprite.play("run_horizontal_down")
 	
-	# Horisontal movement
+	# Sprinting
+	if Input.is_action_pressed("sprint") == true:
+		speedMultiplier = SPRINT
+	else:
+		speedMultiplier = 1
+	# Horizontal movement
 	if direction_x:
-		velocity.x = direction_x * SPEED
+		velocity.x = direction_x * SPEED * speedMultiplier
 		lastDirection = 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	#Vertical movement
 	if direction_y:
-		velocity.y = direction_y * SPEED
+		velocity.y = direction_y * SPEED * speedMultiplier
 		lastDirection = direction_y
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	
 
 	move_and_slide()
+	
+func _ready() -> void:
+	startingPosition = player.position
+	animation.play("RESET")
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	player.z_index = int(player.position.y)
 	if playerHealth > 0:
 		movement()
 	else:
 		dead()
-
-func _on_timer_timeout() -> void:
-	Engine.time_scale = 1.0
-	get_tree().reload_current_scene()
+		
