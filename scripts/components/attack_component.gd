2df224ed87ae8ad21073262a4d31a4d1fbe22865
crystalmathw <121028354+crystalmathw@users.noreply.github.com>
2025-05-06 15:34:11 +0200

    Changed folder structure
    Changed folder structure and switcht from external to build-in scripts

--- scripts/components/attack_component.gd
+++ scripts/components/attack_component.gd
@@ -1,16 +1,11 @@
 extends Area2D
 
-
 @export var attack_damage: float
 @export var knockback_force: float
 @export var stun_time: float
 @export var heal: float
 
-
-
-
 func _on_hitbox_area_entered(area: Area2D) -> void:
-	print(1)
 	if area is HitboxComponent:
 		var hitbox : HitboxComponent = area
 		
